6d9353c02519d26516c9bcbcbc7179cabd2f568f
Edvinas Buinovskis <edvinas.buinovskis@stud.vgtu.lt>
2020-08-25 21:00:12 +0300

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 var passport = require('passport');
+var authenticate = require('../authenticate');
 
 const User = require('../models/user');
+
 router.use(bodyParser.json());
 
 /* GET users listing. */
@@ -31,9 +32,10 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res) => {
