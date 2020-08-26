acc692159edc778fd48d17748705713a9dcb59aa
Edvinas Buinovskis <edvinas.buinovskis@stud.vgtu.lt>
2020-08-26 20:35:34 +0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '337760897587542',
+        clientSecret: '10388017360bccdc6f7156541011b62a'
+    }
 }
