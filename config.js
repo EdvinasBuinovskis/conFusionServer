777bf97c899077a2f7343f507dbe63e46107e4c2
EdvinasBuinovskis <62936669+EdvinasBuinovskis@users.noreply.github.com>
2021-05-05 14:25:35 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientId: '337760897587542',
-        clientSecret: '10388017360bccdc6f7156541011b62a'
-    }
-}
